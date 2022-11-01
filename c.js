7dbb4b8e13c92862d3327ab6fcca2cd3b86132d9
milenaaristizabal <maristizabal978@misena.edu.co>
2022-11-01 12:23:24 -0500

    prueba3 cara/sello

--- c.js
+++ c.js
@@ -1,5 +1,8 @@
-
-		let usuario,nombre;
+let boton = document.getElementById('boton')
+boton.addEventListener('click',()=>{
+		let nombre = document.getElementById('nombre').value
+        let dinero = document.getElementById('cdinero').value
+        let opciones = document.getElementById('opciones').value
         let Suma=0,Resta=0, salir=0;
 
         let maquina=1+(Math.random() * 2);
@@ -7,24 +10,21 @@
             while (salir!=4) {
                 let nombre = prompt("Ingrese su nombre");
 
-            	let dinero = prompt("Dijite la cantidad de dinero que quiere apostar \n");
-                dinero=parseFloat(dinero)
             	alert("La cantidad que ha apostado es: $"+dinero+"\n");
             	
-                prompt("Seleccione una de las opciones \n 1. Cara  \n 2. Sello  \n");
-                usuario=parseFloat(usuario)
 
-                if (usuario==maquina){
+                if (opciones==maquina){
                     alert("Felicidades" +nombre+ "usted ha ganado, la opcion de la maquina fue: "+maquina+ " \n");
                     Suma = Suma+dinero;
                 alert("La cantidad de dinero que ha sumado es: "+Suma+" \n");
                 }
 
                 else{
-                    alert(":( Intentalo de neuvo "+nombre);
+                    alert(":( Perdiste intentalo de nuevo "+nombre);
                     Resta= Resta-Suma;
                 alert("La cantidad de dinero que tiene acumulado hasta ahora es: "+Resta+" \n");
                 }
                 prompt("Si desea continuar selccione 3 y si desea salir del juego seleccione 4");
              }
+            })
         
---
+++ nombre.js
@@ -0,0 +1,27 @@
+
+
+//---------------------------#1 calcular cuantas letras tiene el nombre que se ingrese--------------------------------------
+function saludo(){
+
+    //aqui le estamos diciendo que busque en todo el documento los elementos que tengan el 
+    //id nombre y luego para extraer los elementos se pone el .value
+
+    let nombre = document.getElementById('nombre').value
+    
+    alert("Hola"+nombre)
+}
+
+//---------------------------#2 cambiar de minusculas a mayusculas--------------------------------------
+function validar(){
+    let nombre1=document.getElementById('nombre1').value
+    
+   alert("La cantidad de dinero que ha apostado es $"+nombre1) 
+}
+
+//---------------------------#3 cambiar de mayusculas a minusculas--------------------------------------
+let boton = document.getElementById('boton')
+boton.addEventListener('click',()=>{
+
+let nombre = document.getElementById('1').value
+
+})
